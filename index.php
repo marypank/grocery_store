a6d8edf378f80a31682948e7c96a67d45135f81e
Maria Pankova <qitelopdo@gmail.com>
2023-12-03 20:18:30 +0300

    common nav

--- index.php
+++ index.php
@@ -35,29 +35,7 @@
 
 	?>
     <div style="height: 100vh;">
-        <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
-            <div class="container">
-                <div class="collapse navbar-collapse" id="navbarContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#">Продукты</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Категории</a>
-                        </li>
-                    </ul>
-                    <ul class="navbar-nav">
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Княгиня Ольга (продавщица)</a>
-                            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#">Настройки</a></li>
-                                <li><a class="dropdown-item" href="#">Выйти</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <?php require_once "header.php"; ?>
         <div class="container my-4 py-4">
             <?php if (!is_null($result['message'])) : ?>
                 <div class="alert alert-danger" role="alert"><?= $result['message'] ?></div>
